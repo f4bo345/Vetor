d6100424de542e9896ba9557f68a150d8c26b20e
f4bo345 <58032311+f4bo345@users.noreply.github.com>
2019-11-21 08:25:13 -0300

    Update v4.java

--- v4.java
+++ v4.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-public class Exercicio4 {
+public class v4 {
 	public static void main(String[] args) {
-	Scanner in = new Scanner (System.in);
+	Scanner ler = new Scanner (System.in);
 	 
 	int a[] = new int [10];
 	int i = 0, n = 0;
 	
 	for(i=0; i<10; i++) {
 		System.out.println("Informe o numero");
-		a[i] = in.nextInt();
+		a[i] = ler.nextInt();
 		
 		for(n = 0; n<=a[i]; n++) {
 			if((n%2)==0) {
