ff679cc4346cb02f50964ef3937ae90acc641ef5
f4bo345 <58032311+f4bo345@users.noreply.github.com>
2019-11-21 08:26:22 -0300

    Update v6.java

--- v6.java
+++ v6.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-public class Exercicio6{
+public class v6{
     public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
+    Scanner ler = new Scanner(System.in);
 
     int a[] = new int [11];
 
