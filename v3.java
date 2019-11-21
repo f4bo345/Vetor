686dd5dd7b66ce1e5c540570665b2d99e16f58ee
f4bo345 <58032311+f4bo345@users.noreply.github.com>
2019-11-21 08:24:12 -0300

    Update v3.java

--- v3.java
+++ v3.java
@@ -1,14 +1,14 @@
 import java.util.*;
-public class Exercicio3 {
+public class v3 {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner ler = new Scanner(System.in);
 
 		int a [] = new int [10];
 		int t = 0;
 
 		for(int i = 0; i < 10; i++) {
-			System.out.println("Digite o "+(i+1)+"º valor");
-			a[i] = in.nextInt();
+			System.out.println("Digite o "+(i+1)+"Âº valor");
+			a[i] = ler.nextInt();
 
 			for(int i2 = 1; i2 <= a[i]; i2++) {
 				if(a[i] % i2 == 0) {
@@ -17,10 +17,10 @@ public class Exercicio3 {
 				}
 			}
 			if(t == 2) {
-				System.out.println("É primo");
+				System.out.println("Ã‰ primo");
 			}
 			else {
-				System.out.println("Não é primo");
+				System.out.println("NÃ£o Ã© primo");
 			}
 			t = 0;
 		}
